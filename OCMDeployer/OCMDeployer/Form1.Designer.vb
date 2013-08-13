94c1363249a09b17304d78f795f4c269cbb4bbf2
Mark Sellers <mark@masenterprisesinc.com>
2013-08-12 18:25:35 -0600

    New Utility to deploy Assembly DLL files and HTML files to DEV.

---
+++ OCMDeployer/OCMDeployer/Form1.Designer.vb
@@ -0,0 +1,238 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+		Me.lblFilesInProgress = New System.Windows.Forms.Label()
+		Me.deploymentFilesSelectedLabel = New System.Windows.Forms.Label()
+		Me.Label9 = New System.Windows.Forms.Label()
+		Me.Label8 = New System.Windows.Forms.Label()
+		Me.btnTransfer = New System.Windows.Forms.Button()
+		Me.progressBar1 = New System.Windows.Forms.ProgressBar()
+		Me.overallProgressBar = New System.Windows.Forms.ProgressBar()
+		Me.btnSelectHtmlFilesFolder = New System.Windows.Forms.Button()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.rtbMessage = New System.Windows.Forms.RichTextBox()
+		Me.txtHtmlFilesFolder = New System.Windows.Forms.TextBox()
+		Me.btnSelectAssembliesFolder = New System.Windows.Forms.Button()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.txtAssemblyFilesFolder = New System.Windows.Forms.TextBox()
+		Me.Label4 = New System.Windows.Forms.Label()
+		Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+		Me.lblHtmlFilesTotal = New System.Windows.Forms.Label()
+		Me.SuspendLayout()
+		'
+		'lblFilesInProgress
+		'
+		Me.lblFilesInProgress.AutoSize = True
+		Me.lblFilesInProgress.Location = New System.Drawing.Point(350, 201)
+		Me.lblFilesInProgress.Name = "lblFilesInProgress"
+		Me.lblFilesInProgress.Size = New System.Drawing.Size(115, 13)
+		Me.lblFilesInProgress.TabIndex = 56
+		Me.lblFilesInProgress.Text = "About to transfer files..."
+		Me.lblFilesInProgress.Visible = False
+		'
+		'deploymentFilesSelectedLabel
+		'
+		Me.deploymentFilesSelectedLabel.AutoSize = True
+		Me.deploymentFilesSelectedLabel.Location = New System.Drawing.Point(142, 189)
+		Me.deploymentFilesSelectedLabel.Name = "deploymentFilesSelectedLabel"
+		Me.deploymentFilesSelectedLabel.Size = New System.Drawing.Size(141, 13)
+		Me.deploymentFilesSelectedLabel.TabIndex = 55
+		Me.deploymentFilesSelectedLabel.Text = "number of DLL files selected"
+		'
+		'Label9
+		'
+		Me.Label9.AutoSize = True
+		Me.Label9.Location = New System.Drawing.Point(240, 258)
+		Me.Label9.Name = "Label9"
+		Me.Label9.Size = New System.Drawing.Size(126, 13)
+		Me.Label9.TabIndex = 53
+		Me.Label9.Text = "Overall Transfer Progress"
+		'
+		'Label8
+		'
+		Me.Label8.AutoSize = True
+		Me.Label8.Location = New System.Drawing.Point(3, 258)
+		Me.Label8.Name = "Label8"
+		Me.Label8.Size = New System.Drawing.Size(107, 13)
+		Me.Label8.TabIndex = 52
+		Me.Label8.Text = "Deployment Progress"
+		'
+		'btnTransfer
+		'
+		Me.btnTransfer.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnTransfer.Location = New System.Drawing.Point(6, 189)
+		Me.btnTransfer.Name = "btnTransfer"
+		Me.btnTransfer.Size = New System.Drawing.Size(130, 36)
+		Me.btnTransfer.TabIndex = 51
+		Me.btnTransfer.Text = "Deploy"
+		Me.btnTransfer.UseVisualStyleBackColor = True
+		'
+		'progressBar1
+		'
+		Me.progressBar1.Location = New System.Drawing.Point(6, 274)
+		Me.progressBar1.Name = "progressBar1"
+		Me.progressBar1.Size = New System.Drawing.Size(217, 23)
+		Me.progressBar1.TabIndex = 50
+		'
+		'overallProgressBar
+		'
+		Me.overallProgressBar.Location = New System.Drawing.Point(243, 274)
+		Me.overallProgressBar.Name = "overallProgressBar"
+		Me.overallProgressBar.Size = New System.Drawing.Size(298, 23)
+		Me.overallProgressBar.TabIndex = 49
+		'
+		'btnSelectHtmlFilesFolder
+		'
+		Me.btnSelectHtmlFilesFolder.Location = New System.Drawing.Point(547, 142)
+		Me.btnSelectHtmlFilesFolder.Name = "btnSelectHtmlFilesFolder"
+		Me.btnSelectHtmlFilesFolder.Size = New System.Drawing.Size(24, 22)
+		Me.btnSelectHtmlFilesFolder.TabIndex = 39
+		Me.btnSelectHtmlFilesFolder.Text = "..."
+		Me.btnSelectHtmlFilesFolder.UseVisualStyleBackColor = True
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label1.Location = New System.Drawing.Point(6, 128)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(114, 13)
+		Me.Label1.TabIndex = 38
+		Me.Label1.Text = "HTML Files Folder:"
+		'
+		'rtbMessage
+		'
+		Me.rtbMessage.BackColor = System.Drawing.SystemColors.Control
+		Me.rtbMessage.BorderStyle = System.Windows.Forms.BorderStyle.None
+		Me.rtbMessage.ForeColor = System.Drawing.Color.Navy
+		Me.rtbMessage.Location = New System.Drawing.Point(6, 309)
+		Me.rtbMessage.Name = "rtbMessage"
+		Me.rtbMessage.ReadOnly = True
+		Me.rtbMessage.Size = New System.Drawing.Size(534, 146)
+		Me.rtbMessage.TabIndex = 37
+		Me.rtbMessage.Text = ""
+		'
+		'txtHtmlFilesFolder
+		'
+		Me.txtHtmlFilesFolder.Location = New System.Drawing.Point(7, 144)
+		Me.txtHtmlFilesFolder.Name = "txtHtmlFilesFolder"
+		Me.txtHtmlFilesFolder.Size = New System.Drawing.Size(534, 20)
+		Me.txtHtmlFilesFolder.TabIndex = 35
+		'
+		'btnSelectAssembliesFolder
+		'
+		Me.btnSelectAssembliesFolder.Location = New System.Drawing.Point(546, 96)
+		Me.btnSelectAssembliesFolder.Name = "btnSelectAssembliesFolder"
+		Me.btnSelectAssembliesFolder.Size = New System.Drawing.Size(24, 22)
+		Me.btnSelectAssembliesFolder.TabIndex = 59
+		Me.btnSelectAssembliesFolder.Text = "..."
+		Me.btnSelectAssembliesFolder.UseVisualStyleBackColor = True
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label2.Location = New System.Drawing.Point(5, 82)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(135, 13)
+		Me.Label2.TabIndex = 58
+		Me.Label2.Text = "Assembly DLLs Folder:"
+		'
+		'txtAssemblyFilesFolder
+		'
+		Me.txtAssemblyFilesFolder.Location = New System.Drawing.Point(6, 98)
+		Me.txtAssemblyFilesFolder.Name = "txtAssemblyFilesFolder"
+		Me.txtAssemblyFilesFolder.Size = New System.Drawing.Size(534, 20)
+		Me.txtAssemblyFilesFolder.TabIndex = 57
+		'
+		'Label4
+		'
+		Me.Label4.AutoSize = True
+		Me.Label4.Location = New System.Drawing.Point(13, 13)
+		Me.Label4.Name = "Label4"
+		Me.Label4.Size = New System.Drawing.Size(537, 52)
+		Me.Label4.TabIndex = 63
+		Me.Label4.Text = resources.GetString("Label4.Text")
+		'
+		'BackgroundWorker1
+		'
+		Me.BackgroundWorker1.WorkerReportsProgress = True
+		Me.BackgroundWorker1.WorkerSupportsCancellation = True
+		'
+		'lblHtmlFilesTotal
+		'
+		Me.lblHtmlFilesTotal.AutoSize = True
+		Me.lblHtmlFilesTotal.Location = New System.Drawing.Point(142, 212)
+		Me.lblHtmlFilesTotal.Name = "lblHtmlFilesTotal"
+		Me.lblHtmlFilesTotal.Size = New System.Drawing.Size(151, 13)
+		Me.lblHtmlFilesTotal.TabIndex = 64
+		Me.lblHtmlFilesTotal.Text = "number of HTML files selected"
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(576, 462)
+		Me.Controls.Add(Me.lblHtmlFilesTotal)
+		Me.Controls.Add(Me.Label4)
+		Me.Controls.Add(Me.btnSelectAssembliesFolder)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.txtAssemblyFilesFolder)
+		Me.Controls.Add(Me.lblFilesInProgress)
+		Me.Controls.Add(Me.deploymentFilesSelectedLabel)
+		Me.Controls.Add(Me.Label9)
+		Me.Controls.Add(Me.Label8)
+		Me.Controls.Add(Me.btnTransfer)
+		Me.Controls.Add(Me.progressBar1)
+		Me.Controls.Add(Me.overallProgressBar)
+		Me.Controls.Add(Me.btnSelectHtmlFilesFolder)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.rtbMessage)
+		Me.Controls.Add(Me.txtHtmlFilesFolder)
+		Me.Name = "Form1"
+		Me.Text = "Deploy to DEV"
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+	Friend WithEvents lblFilesInProgress As System.Windows.Forms.Label
+	Friend WithEvents deploymentFilesSelectedLabel As System.Windows.Forms.Label
+	Friend WithEvents Label9 As System.Windows.Forms.Label
+	Friend WithEvents Label8 As System.Windows.Forms.Label
+	Friend WithEvents btnTransfer As System.Windows.Forms.Button
+	Friend WithEvents progressBar1 As System.Windows.Forms.ProgressBar
+	Friend WithEvents overallProgressBar As System.Windows.Forms.ProgressBar
+	Friend WithEvents btnSelectHtmlFilesFolder As System.Windows.Forms.Button
+	Friend WithEvents Label1 As System.Windows.Forms.Label
+	Friend WithEvents rtbMessage As System.Windows.Forms.RichTextBox
+	Friend WithEvents txtHtmlFilesFolder As System.Windows.Forms.TextBox
+	Friend WithEvents btnSelectAssembliesFolder As System.Windows.Forms.Button
+	Friend WithEvents Label2 As System.Windows.Forms.Label
+	Friend WithEvents txtAssemblyFilesFolder As System.Windows.Forms.TextBox
+	Friend WithEvents Label4 As System.Windows.Forms.Label
+	Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+	Friend WithEvents lblHtmlFilesTotal As System.Windows.Forms.Label
+
+End Class
---
+++ OCMDeployer/OCMDeployer/Form1.vb
@@ -0,0 +1,351 @@
+﻿Imports System.Drawing.Drawing2D
+'Imports System.Drawing.Bitmap
+'Imports System.Windows.Media.Imaging
+Imports System.Configuration
+Imports System.Collections.Specialized
+Imports System.IO
+Imports System.Text
+Imports System.Text.RegularExpressions
+Imports System.Reflection
+Imports SFTP_Test
+Imports System.ComponentModel
+
+Public Class Form1
+
+	Private _sftpClient As New OcmSFTPClient()
+
+	' background worker for GUI:
+	Private _Worker As New BackgroundWorker()
+
+	' holds the SFTP specific items:
+	' ************************************************************************************************************
+	' These will need to be moved to the App.Config file before rollout!!!
+	Private _host As String = String.Empty '"files.blackbaudhosting.com"
+	Private _username As String = String.Empty '"MarkSe21195D@BBEC"
+	Private _password As String = "P@ssword2"
+	Private _workingdirectory As String = String.Empty ' "/staging/21195D/ChildPhotos"
+	Private _uploadfile As String = String.Empty ' "C:\OCM\PhotoImports\2013 Photos_Processing_6_25_2013\_Import\C210608.jpg"
+	Private _ftpHtmlFileLocation As String = String.Empty
+	Private _numberOfFiles As Integer = 0
+	Private _ftpAssemblyFileLocation As String = String.Empty
+	'Private _ftpUIModelAssemblyFileLocation As String = String.Empty
+
+	Private _pictureType As String = String.Empty
+	Private _defaultSourceFolder As String = String.Empty
+
+	Private _ftpFolderAlreadyFormatted As Boolean
+
+
+	' holds the files to be transferred:
+	Private _assemblyFiles As String() = {}
+	Private _htmlFiles As String() = {}
+	Private _numberAssemblyFiles As Integer = 0
+	Private _numberHtmlFiles As Integer = 0
+
+	Private Const _messageBoxTitle As String = "Deployment Utility"
+
+
+	Private Sub btnTransfer_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnTransfer.Click
+		btnTransfer.Enabled = False
+		lblFilesInProgress.Visible = True
+
+		'first transfer the DLL Assembly files:
+		'_assemblyFiles = GetAssemblyFiles()   'moved to leave event of folder text box
+		'_htmlFiles = GetHtmlFiles()			'moved to leave event of folder text box
+
+		_numberOfFiles = _numberAssemblyFiles + _numberHtmlFiles '_assemblyFiles.Count + _htmlFiles.Count
+
+		If _numberOfFiles <= 0 Then
+			MessageBox.Show("No files have been found to deploy!", _messageBoxTitle, MessageBoxButtons.OK, MessageBoxIcon.Error)
+		Else
+			'begin the transfer process with assembly files first:
+			If _numberAssemblyFiles > 0 Then
+				TransferFiles(_assemblyFiles, _ftpAssemblyFileLocation)
+			End If
+			If _numberHtmlFiles > 0 Then
+				TransferFiles(_htmlFiles, _ftpHtmlFileLocation)
+			End If
+			Me.overallProgressBar.Value = 100
+			MessageBox.Show("Finished deploying the files to DEV.  Any Packages can be loaded after about 15 minutes...", _messageBoxTitle, MessageBoxButtons.OK, MessageBoxIcon.Information)
+		End If
+
+			'BackgroundWorker1.RunWorkerAsync(_sftpClient)
+	End Sub
+
+	Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+		_ftpFolderAlreadyFormatted = False
+
+		progressBar1.Maximum = 100
+		progressBar1.Style = ProgressBarStyle.Blocks
+		progressBar1.[Step] = 1
+		progressBar1.Value = 0
+		' user can't transfer until they select everything else & process the folder:
+		'btnTransfer.Enabled = False
+
+		GetConfigEntries()
+
+		AddHandler _sftpClient.FileUploadCompleted, AddressOf fileUploadCompleted
+		AddHandler _sftpClient.FileUploading, AddressOf fileUploading
+	End Sub
+
+
+	Private Sub GetConfigEntries()
+		'sets the Photo FTP foldername using the App.config settings entries plus the type of picture title the user selected:
+		' the DEVworkingdirectory = /staging/21195D/ChildPhotos
+		' the PRODworkingdirectory = /Production/21195P/ChildPhotos
+		' the PRODPhotoImportFileLocation = "\\bbhcifs01\clientdata\Production\21195P\ChildPhotos"
+		' the DEVhotoImportFileLocation = "\\bbhcifs01\clientdata\staging\21195D\ChildPhotos"
+		'Const _host As String = "files.blackbaudhosting.com"
+		'Const _username As String = "MarkSe21195D@BBEC"
+		'Const _password As String = "P@ssword2"
+		'Const _workingdirectory As String = "/staging/21195D/ChildPhotos"
+		'Const _uploadfile As String = "C:\OCM\PhotoImports\2013 Photos_Processing_6_25_2013\_Import\C210608.jpg"
+		'Source Folder default location: C:\OCM\PhotoImports
+
+		' This will use an account specially purposed for these SFTP transfers:  OcMFTPUser21195D@BBEC and OcMFTPUser21195P@BBEC
+
+		'_ftpUIModelAssemblyFileLocation = My.Settings.FTPUIModelAssembliesLocation
+		_ftpAssemblyFileLocation = My.Settings.FTPAssembliesLocation
+		_ftpHtmlFileLocation = My.Settings.FTPHTMLFilesLocation
+		_username = My.Settings.FTPUserName
+		_host = My.Settings.FTPHostName
+		_workingdirectory = My.Settings.FTPRootFolderLocation
+		_password = "P@ssword1"
+
+	End Sub
+
+#Region "EventHandlers"
+	Private Sub fileUploadCompleted(ByVal sender As Object, ByVal e As FileUploadCompletedEventArgs)
+		OutputThis("Finished uploading the file!")
+		OutputThis(String.Format("Uploaded {0} {1} {2:#########}", e.UploadedFileName, e.FileNumber, e.BytesSent))
+	End Sub
+
+	Private Sub fileUploading(ByVal sender As Object, ByVal e As FileUploadingEventArgs)
+		'var backgrounder = sender as BackgroundWorker;
+		Dim progress As Integer = 100
+		'progress = (Convert.ToInt32(e.BytesSentSoFar) / Convert.ToInt32(e.FileBytesTotal)) * 100;
+		progress = CInt(Math.Truncate((CSng(e.BytesSentSoFar) / CSng(e.FileBytesTotal)) * 100))
+
+		LogThis(String.Format("Bytes sent so far: {0}", e.BytesSentSoFar))
+		LogThis(String.Format("File Bytes Total: {0}", e.FileBytesTotal))
+		LogThis(String.Format("Progress %{0}", progress))
+		LogThis(String.Format("File {0} of {1}", e.FileNumberOfTotal, e.TotalNumberFilesToTransfer))
+
+		SetControlPropertyValue(lblFilesInProgress, "text", String.Format("Transferring {0} of {1} files...", e.FileNumberOfTotal, e.TotalNumberFilesToTransfer))
+
+		BackgroundWorker1.ReportProgress(progress)
+	End Sub
+
+#End Region
+
+
+	Private Sub OutputThis(ByVal outputMessage As String)
+		'richTextBox1.AppendText(outputMessage)
+		rtbMessage.AppendText(vbNewLine + outputMessage)
+		LogThis(outputMessage)
+	End Sub
+
+	Private Sub LogThis(ByVal logMessage As String)
+		System.Diagnostics.Debug.WriteLine(logMessage)
+	End Sub
+
+#Region "BackgroundWorker Methods"
+	''' <summary>
+	''' Creates the BackgroundWorker with WorkerReportsProgress and 
+	''' WorkerSupportsCancellation set to true. 
+	''' Also creates the three Eventhandlers
+	''' </summary>
+	Public Sub CreateBackgroundWorker()
+		'Set WorkerReportsProgress and WorkerSupportsCancellation to true
+		_Worker.WorkerSupportsCancellation = True
+		_Worker.WorkerReportsProgress = True
+
+		'Generate the EventHandler
+		AddHandler _Worker.RunWorkerCompleted, New RunWorkerCompletedEventHandler(AddressOf Worker_RunWorkerCompleted)
+		AddHandler _Worker.ProgressChanged, New ProgressChangedEventHandler(AddressOf Worker_ProgressChanged)
+		AddHandler _Worker.DoWork, New DoWorkEventHandler(AddressOf Worker_DoWork)
+
+		_Worker.RunWorkerAsync()
+	End Sub
+
+	Private Sub Worker_DoWork(ByVal sender As Object, ByVal e As DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+		' wire up the event listeners to catch the events raised by the client:
+		AddHandler _sftpClient.FileUploadCompleted, AddressOf fileUploadCompleted
+		AddHandler _sftpClient.FileUploading, AddressOf fileUploading
+		'AddHandler _sftpClient.ListingDirectory, AddressOf listingDirectory
+
+		' setup the transfer properties needed by the client:
+		_sftpClient.HostName = _host
+		_sftpClient.UserName = _username
+		_sftpClient.Password = _password
+		_sftpClient.WorkingDirectory = _workingdirectory
+		_sftpClient.UploadFileName = _uploadfile
+		_sftpClient.PhotoFTPFolderName = _ftpHtmlFileLocation  'FormatFTPFolderName()
+
+		' perform the transfer of a single file:
+		'_sftpClient.transferFile();
+
+		' multiple files get this call:
+		_sftpClient.transferFiles(_assemblyFiles)
+	End Sub
+
+	Private Sub Worker_ProgressChanged(ByVal sender As Object, ByVal e As ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+		'Create an instace of the single file progressBar
+		Dim ProgressBarSingle As ProgressBar = DirectCast(Me.Controls("progressBar1"), ProgressBar)
+
+		'Increase the value of the progressBar by e.ProgressPercentage
+		ProgressBarSingle.Value = e.ProgressPercentage
+
+		Dim ProgressBarOverall As ProgressBar = DirectCast(Me.Controls("overallProgressBar"), ProgressBar)
+
+		'Calculate by whic the Overall ProgressBar needs to be increased,
+		'so that we get a smooth rise.
+		Dim PercentRise As Integer = 100 \ _numberOfFiles
+
+		' only increment the overall bar if this single file is done
+		If ProgressBarSingle.Value = 100 Then
+			'Duno why it has to be here, but if it isn't the progressBarSingle
+			'doesn't stop working. So leave it here with no code...
+			If ProgressBarOverall.Value >= 100 Then
+			Else
+				'Set the SingleFile progressBar to 0, so it cann be filled again
+				'ProgressBarSingle.Value = 0;
+
+				'This is the "catch" part, so the programm doesn't throw up an error
+				If ProgressBarOverall.Value + PercentRise >= 100 Then
+					'Set both progress bars to 100 so it looks better.
+					ProgressBarOverall.Value = 100
+					ProgressBarSingle.Value = 100
+				Else
+					'increase the Global progressBar by the calculated amount for each file
+					ProgressBarOverall.Value = ProgressBarOverall.Value + PercentRise
+				End If
+			End If
+		End If
+	End Sub
+
+	Private Sub Worker_RunWorkerCompleted(ByVal sender As Object, ByVal e As RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+		Dim ProgressBarSingle As ProgressBar = DirectCast(Me.Controls("progressBar1"), ProgressBar)
+		ProgressBarSingle.Value = 100
+
+		SetControlPropertyValue(overallProgressBar, "value", 100)
+
+		SetControlPropertyValue(btnTransfer, "enabled", True)
+
+		SetControlPropertyValue(lblFilesInProgress, "text", "All files transferred!")
+
+		MessageBox.Show("Image files have been transferred. You may now close this application...", _messageBoxTitle, MessageBoxButtons.OK, MessageBoxIcon.Information)
+	End Sub
+#End Region
+
+
+
+	''' <summary>
+	''' These are used to manage the UI controls from the worker thread
+	''' </summary>
+	''' <param name="oControl"></param>
+	''' <param name="propName"></param>
+	''' <param name="propValue"></param>
+	Private Delegate Sub SetControlValueCallback(ByVal oControl As Control, ByVal propName As String, ByVal propValue As Object)
+	Private Sub SetControlPropertyValue(ByVal oControl As Control, ByVal propName As String, ByVal propValue As Object)
+		If oControl.InvokeRequired Then
+			Dim d As New SetControlValueCallback(AddressOf SetControlPropertyValue)
+			oControl.Invoke(d, New Object() {oControl, propName, propValue})
+		Else
+			Dim t As Type = oControl.[GetType]()
+			Dim props As PropertyInfo() = t.GetProperties()
+			For Each p As PropertyInfo In props
+				If p.Name.ToUpper() = propName.ToUpper() Then
+					p.SetValue(oControl, propValue, Nothing)
+				End If
+			Next
+		End If
+	End Sub
+
+	Private Function GetAssemblyFiles() As String()
+		Dim filesList As String()
+		If Not Me.txtAssemblyFilesFolder.Text.EndsWith("\") Then
+			txtAssemblyFilesFolder.Text = txtAssemblyFilesFolder.Text + "\"
+		End If
+		filesList = Directory.GetFiles(Me.txtAssemblyFilesFolder.Text, "*.dll")
+		_numberAssemblyFiles = filesList.Count()
+		deploymentFilesSelectedLabel.Text = String.Format("{0} Assembly files ready to transfer.", _numberAssemblyFiles)
+
+		Return filesList
+	End Function
+
+	Private Function GetHtmlFiles() As String()
+		Dim filesList As String()
+		If Not Me.txtHtmlFilesFolder.Text.EndsWith("\") Then
+			txtHtmlFilesFolder.Text = txtHtmlFilesFolder.Text + "\"
+		End If
+		filesList = Directory.GetFiles(Me.txtHtmlFilesFolder.Text, "*.html")
+		_numberHtmlFiles = filesList.Count()
+		lblHtmlFilesTotal.Text = String.Format("{0} Html files ready to transfer.", _numberHtmlFiles)
+
+		Return filesList
+	End Function
+
+	Private Sub TransferFiles(ByVal filesToTransfer As String(), ByVal transferLocation As String)
+		If _sftpClient Is Nothing Then
+			_sftpClient = New OcmSFTPClient()
+		End If
+
+		If Not _sftpClient Is Nothing Then
+			' setup the transfer properties needed by the client:
+			_sftpClient.HostName = _host
+			_sftpClient.UserName = _username
+			_sftpClient.Password = _password
+			_sftpClient.WorkingDirectory = _workingdirectory
+			_sftpClient.UploadFileName = _uploadfile
+			_sftpClient.PhotoFTPFolderName = transferLocation
+
+			' multiple files get this call:
+			_sftpClient.transferFiles(filesToTransfer)
+		End If
+
+	End Sub
+
+	Private Sub btnSelectAssembliesFolder_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSelectAssembliesFolder.Click
+		Dim dlg As FolderBrowserDialog
+
+		dlg = New FolderBrowserDialog()
+		If dlg.ShowDialog() = Windows.Forms.DialogResult.OK Then
+			txtAssemblyFilesFolder.Text = dlg.SelectedPath
+		End If
+
+		If Not String.IsNullOrEmpty(txtAssemblyFilesFolder.Text) Then
+			_assemblyFiles = GetAssemblyFiles()
+		End If
+
+		txtHtmlFilesFolder.Focus()
+	End Sub
+
+	Private Sub btnSelectHtmlFilesFolder_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSelectHtmlFilesFolder.Click
+		Dim dlg As FolderBrowserDialog
+
+		dlg = New FolderBrowserDialog()
+		If dlg.ShowDialog() = Windows.Forms.DialogResult.OK Then
+			txtHtmlFilesFolder.Text = dlg.SelectedPath
+		End If
+
+		If Not String.IsNullOrEmpty(txtHtmlFilesFolder.Text) Then
+			_htmlFiles = GetHtmlFiles()
+		End If
+
+		btnTransfer.Focus()
+
+	End Sub
+
+	Private Sub txtAssemblyFilesFolder_Leave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtAssemblyFilesFolder.Leave
+		If Not String.IsNullOrEmpty(txtAssemblyFilesFolder.Text) Then
+			_assemblyFiles = GetAssemblyFiles()
+		End If
+	End Sub
+
+	Private Sub txtHtmlFilesFolder_Leave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtHtmlFilesFolder.Leave
+		If Not String.IsNullOrEmpty(txtHtmlFilesFolder.Text) Then
+			_htmlFiles = GetHtmlFiles()
+		End If
+	End Sub
+End Class
